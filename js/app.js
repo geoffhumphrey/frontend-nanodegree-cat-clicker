b5b79b7ab35df5cc750fd0f87e9b89fb052207fc
Geoff Humphrey <ghumphrey1971@gmail.com>
2018-01-30 21:36:12 -0700

    v2 - Two Cats Working w/ Names

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
-    var counter = 0;
-    $("#counter").append(counter.toString());
+
+    var counterCat1 = 0;
+    var counterCat1Name = "Mike";
+    $("#cat-clicker-name-1").append(counterCat1Name.toString());
+    $("#cat-counter-1").append(counterCat1.toString());
     $("#cat-clicker-1").click(function() {
-        counter += 1;
-        $("#counter").empty();
-        $("#counter").append(counter.toString());
+        counterCat1 += 1;
+        $("#cat-counter-1").empty();
+        $("#cat-counter-1").append(counterCat1.toString());
     });
+
+    var counterCat2 = 0;
+    var counterCat2Name = "Jorge";
+    $("#cat-clicker-name-2").append(counterCat2Name.toString());
+    $("#cat-counter-2").append(counterCat2.toString());
+    $("#cat-clicker-2").click(function() {
+        counterCat2 += 1;
+        $("#cat-counter-2").empty();
+        $("#cat-counter-2").append(counterCat2.toString());
+    });
+
 });
